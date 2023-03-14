98eba97a0765c397ef4e6d7910a3fe5531bbe6fc
Sowmya <sowmya.codes@gmail.com>
2023-03-14 12:48:20 -0700

    Add html template and view for schedule manager with support for viewing daily schedules grouped by month

--- schedule_manager/views.py
+++ schedule_manager/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import DailySchedule
+from datetime import datetime
+import pprint
 
 # Create your views here.
 def daily_schedule_list(request):
-    return render(request, 'schedule_manager/daily_schedule.html', {})
+    # Fetch all schedule objects from the model, reverse chronologically ordered
+    schedules = DailySchedule.objects.order_by('-date')
+    # Construct a dictionary grouped by month-year
+    monthly_grouping = {}
+    for schedule in schedules:
+        month_year = schedule.date.strftime('%b')  + " " + str(schedule.date.year) 
+        if month_year not in monthly_grouping:
+            monthly_grouping[month_year] = [schedule]
+        else:
+            monthly_grouping[month_year].append(schedule)
+
+    context = {"monthly_grouping": monthly_grouping}
+    return render(request, 'schedule_manager/daily_schedule.html', context)
